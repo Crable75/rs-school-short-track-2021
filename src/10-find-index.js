de9428b4d05c53e8816d12ce018ea04f00fbeac9
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2021-04-08 21:17:25 +0300

    feat: resolve 10-find-index-tests

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,20 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(arr, value) {
+  let inSearch = 0;
+  let outSearch = arr.length - 1;
+  while (inSearch <= outSearch) {
+    let middle = Math.floor((inSearch + outSearch) / 2);
+    if (arr[middle] === value) {
+      return middle;
+    } else if (arr[middle] < value) {
+      inSearch = middle + 1;
+    } else {
+      outSearch = middle - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;
