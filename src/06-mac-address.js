caba1919d1eb658ceff2def5a37b62c89fa32d27
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2021-04-08 20:02:14 +0300

    feat: resolve 06-mac-address

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  return /^[0-9a-f]{1,2}([.:-])(?:[0-9a-f]{1,2}\1){4}[0-9a-f]{1,2}$/gmi.test(n);
 }
 
 module.exports = isMAC48Address;
+// console.log(isMAC48Address('00-1B-63-84-45-E6'));
