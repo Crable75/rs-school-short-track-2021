cb0001a43719bf439c200f35df4890685d660419
Volodymyr Ishchuk <vladimir.ischuk@gmail.com>
2021-04-08 21:52:12 +0300

    feat: resolve 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let res = '';
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === str[i-1]) {
+      count++;
+    } else {
+      res = `${res}${count}${str[i-1]}`;
+      count = 1;
+    }
+    if (i === str.length - 1) {
+      res = `${res}${count}${str[i]}`;
+    }
+  }
+  return res.replace(/1/g, '');
 }
 
 module.exports = encodeLine;
